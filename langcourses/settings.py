faae361a9a785bbe793444a5dfe33d35f087b0fc
Владимир Бибин <devbibinva@gmail.com>
2017-10-20 15:04:48 +0300

    STATIC ROOT added for heroku deployment

--- langcourses/settings.py
+++ langcourses/settings.py
@@ -43,11 +43,15 @@ else:
         }
     }
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ALLOWED_HOSTS = []
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Application definition
 
