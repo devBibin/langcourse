32e948fd619072b8c9985481a88e773be9a24638
Владимир Бибин <devbibinva@gmail.com>
2017-10-20 16:29:10 +0300

    Travis script was added

--- appbase/views.py
+++ appbase/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Настя котеночек!")
+    return HttpResponse("Hello world!")
 
 # Create your views here.
