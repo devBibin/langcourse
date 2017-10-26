1a4383bdcee88ae0e866f560884fc7f5e86e1790
Владимир Бибин <devbibinva@gmail.com>
2017-10-26 15:55:03 +0300

    Test database was changed

--- langcourses/settings.py
+++ langcourses/settings.py
@@ -48,7 +48,7 @@ else:
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = ['gentle-journey-72908.herokuapp.com']
+ALLOWED_HOSTS = ['gentle-journey-72908.herokuapp.com', 'localhost']
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
